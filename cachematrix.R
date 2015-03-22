9b65bab4a3ccc1951bf166a64323ecf8d3a61948
Pramod Kumar <Pramod.Kumar1@astrazeneca.com>
2015-03-22 00:43:26 -0400

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a special matrix that can cache its inverse. cacheSolve takes as input 
+## the special matrix created by makecacheMatrix and returns the inverse of the matrix. If a valid
+## inverse has been cached, it is returned, otherwise an inverse is created using the solve function and is cached 
+## before returning.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix that can cache its inverse. This function returns a list
+## of four functions that can be used to get ans set the matrix as well as it's inverse. It takes 
+## as input a matrix whose inverse esists.
 
 makeCacheMatrix <- function(x = matrix()) {
   invx <- NULL
@@ -19,7 +23,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes as input the special matrix created by makecacheMatrix and returns the inverse of the matrix.
+## If a valid inverse is cached, it is returned else inverse is created using the solve function and is cached 
+## before returning.
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
